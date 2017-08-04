9adfeb9369f988969aba4ed6fa459c61f5aa075e
Greg Susko <gsusko1@gmail.com>
2017-08-04 11:30:59 -0700

    setup search and voice components

---
+++ react-client/src/components/Voice.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Voice = (props) => (
+  <div>
+    Voice
+  </div>
+)
+
+export default Voice;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import Search from './components/Search.jsx';
+import Voice from './components/Voice.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       items: []
     }
   }
 
   componentDidMount() {
     $.ajax({
-      url: '/items', 
+      url: '/items',
       success: (data) => {
         this.setState({
           items: data
@@ -25,10 +28,15 @@ class App extends React.Component {
     });
   }
 
+  //search function
+
+
   render () {
     return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+      <h1>Spotify Player</h1>
+      <div><Search/></div>
+      <div><Voice/></div>
+      <div><List items={this.state.items}/></div>
     </div>)
   }
 }
